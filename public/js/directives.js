3c534895e342f931ab4d8df256ebb7465b3fa167
Julian Jelfs <julian.jelfs@gmail.com>
2013-05-23 20:58:44 +0000

    rejig auth

--- public/js/directives.js
+++ public/js/directives.js
@@ -3,9 +3,27 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+angular.module('myApp.directives', [])
+  .directive('helpmegooutApp', function() {
+    return {
+      restrict: 'C',
+      link: function(scope, elem, attrs) {
+        elem.removeClass('waiting-for-angular');
+        
+        var login = elem.find('#login-holder');
+        var main = elem.find('#main');
+        
+        login.hide();
+        
+        scope.$on('event:auth-loginRequired', function() {
+          login.slideDown('slow', function() {
+            main.hide();
+          });
+        });
+        scope.$on('event:auth-loginConfirmed', function() {
+          main.show();
+          login.slideUp();
+        });
+      }
+    }
+  });
