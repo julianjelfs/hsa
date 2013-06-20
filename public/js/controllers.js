6585633a855541cb9738c3ba8eaf1cd8d189954e
Julian Jelfs <julian.jelfs@gmail.com>
2013-06-20 19:27:55 +0000

    more or less finished the ui side of the contact form

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -11,11 +11,28 @@ function ContactCtrl($scope, $http, $routeParams, $location, contact){
     flag = parseInt($routeParams.flag);
   }
   $scope.to = [];
+  $scope.members = [];
   
   for(var prop in contact) {
     if((contact[prop].flag & flag) === contact[prop].flag) {
       $scope.to.push(contact[prop]);
+    } else {
+      $scope.members.push(contact[prop]);  
     }
+  }
+  
+  $scope.addRecipient = function(m){
+    $scope.to.push(m);
+    var i = $scope.members.indexOf(m);
+    if(i >= 0)
+      $scope.members.splice(i,1);
+  }
+  
+  $scope.removeRecipient = function(m){
+    $scope.members.push(m);
+    var i = $scope.to.indexOf(m);
+    if(i >= 0)
+      $scope.to.splice(i,1);
   }
   
   $scope.send = function(c) {
