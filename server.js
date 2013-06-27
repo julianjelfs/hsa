66b35bc38c5225ea0d898e5db07f429cd24b59df
Julian Jelfs <julian.jelfs@gmail.com>
2013-06-27 10:29:22 +0000

    trying some more tweaks to get things running on azure

--- server.js
+++ server.js
@@ -78,6 +78,7 @@ app.get("*", function (req, res) {
 
 // Start server
 
-app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
